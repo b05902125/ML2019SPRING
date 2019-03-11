cf5270cbceed7fb204ea6035bd8d2e4bf16053be
b05902125 <b05902125@ntu.edu.tw>
2019-03-11 18:10:42 +0800

    123

--- hw1_final/hw1.sh
+++
@@ -1,4 +0,0 @@
-testing_data=$1
-output_file=$2
-
-python3 pm_hw1_test.py $testing_data $output_file
--- hw1_final/hw1_best.sh
+++
@@ -1,4 +0,0 @@
-testing_data=$1
-output_file=$2
-
-python3 ova_hw1_test.py $testing_data $output_file
